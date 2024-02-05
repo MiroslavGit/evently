b8e455c113f0edcd086c399d832017235919586a
MiroslavGit <miroslav2022@gmail.com>
2024-02-05 17:36:46 +0100

    added public images and icons, setup metadata

---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Ahoj</h1>
+    </main>
+  );
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <main>
-      <Button variant="destructive" className="px-11">Click me</Button>
-    </main>
-  );
-}
