206a344a565cde8c0389865cd0123b9a811fdd5d
MiroslavGit <miroslav2022@gmail.com>
2024-02-15 17:35:54 +0100

    added create event page, working on EventForm.tsx, added zod validator

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,10 +2,10 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+let cached = (global as any).mongoose || { connection: null, promise: null };
 
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
+  if (cached.connection) return cached.connection;
 
   if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
@@ -14,7 +14,7 @@ export const connectToDatabase = async () => {
     bufferCommands: false,
   })
 
-  cached.conn = await cached.promise;
+  cached.connection = await cached.promise;
 
-  return cached.conn;
+  return cached.connection;
 }
